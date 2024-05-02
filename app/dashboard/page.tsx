05e60e9b420cea39131e958d241d1d4e02211a74
Hersh Kataria <hershkat@hershs-mbp.home>
2024-05-02 12:46:35 +0100

    Add dashboard elements, fetch data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,28 @@
-export default function Page() {
+import CardWrapper from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import { fetchCardData, fetchRevenue } from '@/app/lib/data';
+import { fetchLatestInvoices } from '@/app/lib/data';
+
+export default async function Page() {
+    const revenue = await fetchRevenue();
+    const invoices = await fetchLatestInvoices();
+    const cardData = await fetchCardData();
+
     return (
-        <p>
-            Wagwan this is ur dashboard g
-        </p>
+        <main>
+        <h1 className={`${lusitana.className} mb-4 text-xl md:text-2x1`}>
+            Dashboard
+        </h1>
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+
+        </div>
+        <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+            <RevenueChart revenue={revenue}/>
+            <LatestInvoices latestInvoices={invoices}/>
+            <CardWrapper {...cardData}/>
+        </div>
+        </main>
     );
 }
